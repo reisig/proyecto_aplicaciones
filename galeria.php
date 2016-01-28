43a33768ca13eba30a23065d720476db76d1c9ea
Caraya0100 <caraya0100@gmail.com>
2016-01-28 11:46:19 -0300

    Plugins que se usaran en el proyecto.
    El codigo no funciona, por lo que debe ser arreglado.

---
+++ galeria.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Este archivo muestra la galeria de imagnes (repositorio).
+ *
+ * @package RepBiologia
+ * @since RepBiologia 1.0
+ */
+
+require_once(__DIR__. '/cabecera.php');
+
+ ?>
+
+
+
+ <?php require_once(DIR. '/pie-pagina.php');  ?>
