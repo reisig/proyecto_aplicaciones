5f4e301e99048e2c79cfeade0266c0d940648bff
reisig <sopsiud@gmail.com>
2016-02-13 04:22:55 -0300

    agregado instalar.php

--- instalar.php
+++ instalar.php
@@ -1,40 +1,49 @@
 <?php
 	/**
-	* Clase encargada de la instalación de la 
+	* Clase encargada de la instalación de la
 	*	base de datos en el servidor
 	*
 	* @package RepBiologia
 	* @since RepBiologia 1.0
 	*/
 
-	require_once(__DIR__.'/scripts/cargar.php');
-
 ?>
 
 <!DOCTYPE html>
 <html>
-	<meta charset="UTF-8"> 
 	<head>
+		<meta charset="UTF-8"> 
 		<title>Instalación base de datos MySQL</title>
-		<?php agregar_css('estilo.css'); ?>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<script type="text/javascript" src="js/jquery-1.12.0.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/ScriptInstalacion.js"></script>
 	</head>
 	<body>
-		<div id = "header">	</div>
-		<div> <h1>Este es el módulo de instalación de la base de datos MySQL</h1>	</div>
-		<form id = 'FormInstalacion' align ="center">
-		    <fieldset>
+		<div id="mhead"><h2>Instalación Base de datos MySQL</h2></div>
+		<form id="FormInstalacion">
+			<ul id="progressbar">
+				<li class="active">Conexión</li>
+				<li>Base de datos</li>
+				<li>Administrador</li>
+			</ul>
+		    <fieldset id = "conexion">
 				<h2 class="fs-title">Conexión</h2>
-				<h3 class="fs-subtitle">Conexión a la base de datos</h3>
+				<h3 class="fs-subtitle">Conexión al Servidor</h3>
 				<input type="text" name="host" id="host" placeholder="Host" />
-				<input type="text" name="puerto"  id="puerto" placeholder="Puerto" />
-				<input type="text" name="dbuser" id= "dbuser" placeholder="Usuario" />
-				<input type="password" name="dbpassword" id ="dbpassword" placeholder="Contraseña" />
-				<input type="button" name="checkdb" class="check action-button" value="Verificar conexión" />
-			</fieldset>	
+				<input type="text" name="port"  id="port" placeholder="Puerto" />
+				<input type="text" name="dbuser" id="dbuser" placeholder="Usuario" />
+				<input type="password" name="dbpass" id ="dbpass" placeholder="Contraseña" />
+				<input type="button" name="checkdb" id= "checkdb" class="check action-button" value="Verificar conexión" />
+				<input type="button" name="next" id="b1" class="next action-button" value="Siguiente" />
+			</fieldset>
 			<fieldset>
 				<h2 class="fs-title">Creación base de datos</h2>	
-				<input type="text" name="dbname" id = "dbname" placeholder="Nombre" />
+				<input type="text" name="dbname" id="dbname" placeholder="Nombre" />
 				<input type="text" name="dbprefix" id="dbprefix" placeholder="Prefijo para las tablas" />
+				<input type="button" name="previous" class="previous action-button" value="Anterior" />
+				<input type="button" name="next" id="b2" class="next action-button" value="Siguiente" />
 			</fieldset>
 			<fieldset>
 				<h3 class="fs-title">Administrador</h3>
@@ -47,9 +56,9 @@
 				<input type="text" name="correo" id="correo" placeholder="Correo" />
 				<input type="password" name="password" id="password" placeholder="Contraseña" />
 				<input type="password" name="cpassword" id="cpassword" placeholder="Repita contraseña" />
-			</fieldset>	
-			<input type="submit" name="submit" class="submit action-button" value="Confirmar" />
+				<input type="button" name="previous" class="previous action-button" value="Anterior" />
+				<input type="button" name="submit" id="submit" class="submit action-button" value="Confirmar" />
+			</fieldset>
 		</form>
-		<div id = "footer">	</div>
 	</body>
 </html>
