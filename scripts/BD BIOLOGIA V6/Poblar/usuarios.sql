a7097ed3238829afb578502498c369adfea4604c
gonen09 <gonen.rt@gmail.com>
2016-02-18 00:46:37 -0300

    Base de datos v6 + script para crear y poblar

---
+++ scripts/BD BIOLOGIA V6/Poblar/usuarios.sql
@@ -0,0 +1,5 @@
+/*CREAR USUARIOS*/
+
+insert into usuarios values ('15302958-k','Francisco','Lopez','Miranda','password','flopez@userena.cl','PROFESOR');
+insert into usuarios values ('11363588-6','Cristian','Plaza','Miraflorez','password','cplaza@userena.cl','ALUMNO');
+insert into usuarios values ('17170307-7','Richard','Ayala','Mondaca','password','rayala@userena.cl','ADMINISTRADOR');
