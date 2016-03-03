f63e0d1541e293e9c8ccf4c388c29cbff048ee92
Gabriela Barrera <metallica_716@hotmail.com>
2016-03-03 20:22:41 -0300

    Se agregaron sólo las dependencias COMENTADAS en el archivo /scripts/cargar.php. Cada encargado del módulo debe encargarse de agregar las dependencias en el orden correcto, además se agregaron los css y js en el archivo cabecera.php.

--- cabecera.php
+++ cabecera.php
@@ -1,34 +1,37 @@
 <?php 
-/**
- * Cabecera para la pagina.
- *
- * @package RepBiologia
- * @since RepBiologia 1.0
- */
+    /**
+     * Cabecera para la pagina.
+     *
+     * @package RepBiologia
+     * @since RepBiologia 1.0
+     */
 
-
-//cargamos las dependencias.
-require_once(__DIR__. '/scripts/cargar.php');
-
+    //cargamos las dependencias.
+    require_once(__DIR__. '/scripts/cargar.php');
  ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Repositorio - Departamento de Biologia</title>
-	<?php 
-		agregar_css('estilo.css');
-		agregar_css('photoswipe.css');
-		agregar_css('default-skin/default-skin.css');
-		agregar_js('jquery-1.12.0.min.js');
-		agregar_js('bootstrap.min.js');
-		agregar_js('masonry.pkgd.min.js');
-		agregar_js('photoswipe.min.js');
-		agregar_js('photoswipe-ui-default.min.js');
-	 ?>
-</head>
-<body>
+    <head>
+        <title>Repositorio - Departamento de Biologia</title>
 
-</body>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/photoswipe.css">
+        <link rel="stylesheet" type="text/css" href="css/default-skin/default-skin.css">
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+        <script type="text/javascript" src="js/jquery-1.12.0.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/masonry.pkgd.min.js"></script>
+        <script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+        <script type="text/javascript" src="js/photoswipe.min.js"></script>
+        <script type="text/javascript" src="js/photoswipe-ui-default.min.js"></script>
+        <script type="text/javascript" src="js/init.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>  
+    </head>
+    
+    <body></body>   
 </html>
 
