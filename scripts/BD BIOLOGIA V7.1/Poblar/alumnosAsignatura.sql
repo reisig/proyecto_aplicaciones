aee73bf195a7f154c3239e421e266784bb9b21d4
gonen09 <gonen.rt@gmail.com>
2016-03-10 00:18:07 -0300

    Agregando scripts bd prueba, alumnos en asignaturas , tabla UsuarioAsignatura

---
+++ scripts/BD BIOLOGIA V7.1/Poblar/alumnosAsignatura.sql
@@ -0,0 +1,24 @@
+/*Asignatura 1*/
+insert into UsuarioAsignatura values ('5087191-6',1);
+insert into UsuarioAsignatura values ('21483162-7',1);
+insert into UsuarioAsignatura values ('9061535-1',1);
+insert into UsuarioAsignatura values ('21900505-9',1);
+insert into UsuarioAsignatura values ('13623266-5',1);
+insert into UsuarioAsignatura values ('17496956-6',1);
+
+/*Asignatura 2*/
+insert into UsuarioAsignatura values ('7785820-2',2);
+insert into UsuarioAsignatura values ('18609017-9',2);
+insert into UsuarioAsignatura values ('20507089-3',2);
+insert into UsuarioAsignatura values ('8096473-0',2);
+insert into UsuarioAsignatura values ('19422654-3',2);
+insert into UsuarioAsignatura values ('12963706-4',2);
+
+/*Asignatura 3*/
+insert into UsuarioAsignatura values ('5615390-k',3);
+insert into UsuarioAsignatura values ('20554509-3',3);
+insert into UsuarioAsignatura values ('22883530-7',3);
+insert into UsuarioAsignatura values ('20192377-8',3);
+insert into UsuarioAsignatura values ('13630076-8',3);
+insert into UsuarioAsignatura values ('20141419-9',3);
+insert into UsuarioAsignatura values ('14948123-0',3);
